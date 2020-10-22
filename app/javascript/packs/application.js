8c1ee8ddeebce7c7306bb744dcb8dd2a72a85779
Julien Bouland <bouland.julien@gmail.com>
2020-10-22 21:52:49 +0200

    Split home page template's content into partials

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,24 +17,25 @@ import { randomColor } from '../posts/style';
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 randomColor();
-// $(document).on('turbolinks:load', function() {
-//   // when a post is clicked, show its full content in a modal window
-//   $("body").on( "click", ".single-post-card, .single-post-list", function() {
-//     var posted_by = $(this).find('.post-content .posted-by').html();
-//     var post_heading = $(this).find('.post-content h3').html();
-//     var post_content = $(this).find('.post-content p').html();
-//     var interested = $(this).find('.post-content .interested').attr('href');
-//     $('.modal-header .posted-by').text(posted_by);
-//     $('.loaded-data h3').text(post_heading);
-//     $('.loaded-data p').text(post_content);
-//     $('.loaded-data .interested a').attr('href', interested);
-//     $('.myModal').css("display", "block");
-//     // $('.myModal').modal('show');
-//   });
-// });
+$(document).on('turbolinks:load', function() {
+  // when a post is clicked, show its full content in a modal window
+  $("body").on( "click", ".single-post-card, .single-post-list", function() {
+    var posted_by = $(this).find('.post-content .posted-by').html();
+    var post_heading = $(this).find('.post-content h3').html();
+    var post_content = $(this).find('.post-content p').html();
+    var interested = $(this).find('.post-content .interested').attr('href');
+    $('.modal-header .posted-by').text(posted_by);
+    $('.loaded-data h3').text(post_heading);
+    $('.loaded-data p').text(post_content);
+    $('.loaded-data .interested a').attr('href', interested);
+    $('.myModal').css("display", "block");
+    // $('.myModal').modal('show');
+  });
+});
 $('#myModal').on('shown.bs.modal', function () {
   var posted_by = $(this).find('.post-content .posted-by').html();
   var post_heading = $(this).find('.post-content h3').html();
   var post_content = $(this).find('.post-content p').html();
   var interested = $(this).find('.post-content .interested').attr('href');
+  $('body').trigger('focus')
 });
