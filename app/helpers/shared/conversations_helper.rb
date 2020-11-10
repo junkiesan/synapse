50945a00d7e5b2b89f7a61f8aa83aedcabfa8790
Julien Bouland <bouland.julien@gmail.com>
2020-11-10 17:46:55 +0100

    Create a link on the navigation bar to open a group conversation

--- app/helpers/shared/conversations_helper.rb
+++ app/helpers/shared/conversations_helper.rb
@@ -59,12 +59,26 @@ module Shared::ConversationsHelper
     # return all contacts, except the opposite user of the chat
     contacts.delete_if {|contact| contact.id == recipient.id }
   end
-  
+
   def create_group_conv_partial_path(contact)
     if recipient_is_contact?
       'private/conversations/conversation/heading/create_group_conversation'
     else
       'shared/empty_partial'
+    end
+  end
+
+  def group_conv_seen_status(conversation, current_user)
+    # if the current_user is nil, it means that the helper is called from the service
+    # return an empty string
+    if current_user == nil
+      ''
+    else
+      # if the last message of the conversation is not created by this user
+      # and is unseen, return an unseen-conv value
+      not_created_by_user = conversation.messages.last.user_id != current_user.id
+      seen_by_user = conversation.messages.last.seen_by.include? current_user.id
+      not_created_by_user && seen_by_user == false ? 'unseen-conv' : ''
     end
   end
 
