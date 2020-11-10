74633949ca6ac385af08cc688cdb52a23aa94891
Julien Bouland <bouland.julien@gmail.com>
2020-11-10 17:51:08 +0100

    - Create a _conversation.html.erb inside the group/conversations - Define a add_people_to_group_conv_list and write specs for it

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,7 @@ require 'navigation_helper.rb'
 include PostsHelper
 include Private::ConversationsHelper
 include Private::MessagesHelper
+include Group::ConversationsHelper
 
 module ApplicationHelper
   include NavigationHelper
