217ce62d0b1c01ad576b13864b670becd68ba684
Julien Bouland <bouland.julien@gmail.com>
2020-11-10 17:40:59 +0100

    Define a set_user_data private method in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :opened_conversations_windows
   before_action :all_ordered_conversations
+  before_action :set_user_data
 
   def opened_conversations_windows
     if user_signed_in?
@@ -29,5 +30,17 @@ class ApplicationController < ActionController::Base
       @all_conversations = OrderConversationsService.new({user: current_user}).call
     end
   end
-  
+
+  private
+
+  def set_user_data
+    if user_signed_in?
+      gon.group_conversations = current_user.group_conversations.ids
+      gon.user_id = current_user.id
+      cookies[:user_id] = current_user.id if current_user.present?
+      cookies[:group_conversations] = current_user.group_conversations.ids
+    else
+      gon.group_conversations = []
+    end
+  end
 end
