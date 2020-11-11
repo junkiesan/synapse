23190324efa2798afd39989941c0acbe01723db8
Julien Bouland <bouland.julien@gmail.com>
2020-11-11 18:13:43 +0100

    Define private_conversations_windows and group_conversations_windows helper methods inside the ApplicationHelper and test them

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,12 @@ include Group::MessagesHelper
 
 module ApplicationHelper
   include NavigationHelper
+
+  def private_conversations_windows
+    params[:controller] != 'messengers' ? @private_conversations_windows : []
+  end
+  
+  def group_conversations_windows
+    params[:controller] != 'messengers' ? @group_conversations_windows : []
+  end
 end
